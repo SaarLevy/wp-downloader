c494f2f3c09548a3bf46cca5a379a12f178fb5cd
Saar Levy <saar.levy0@gmail.com>
2017-06-02 19:13:41 +0300

    Added function to get links from subreddit and function for downlading images

---
+++ src/main.py
@@ -0,0 +1,4 @@
+import os
+import reddit
+
+path = '/home/saar/Pictures/wp-test'
--- src/reddit.py
+++ src/reddit.py
@@ -1,10 +1,33 @@
 import praw
 import requests
-from config import reddit as cr
+import os
+import imgur
+import shutil
+from config import reddit as cr # Not commited
 
 reddit = praw.Reddit(client_id= cr['id'],
                      client_secret= cr['secret'],
-                     redirect_uri= "https//localhost:8080",
+                     redirect_uri= "https://localhost:8080",
                      user_agent= 'Downloads wallpapers from r/wallpapers')
 
-print(reddit.auth.url(['identity'], '...', 'permanent'))
+
+def crawl_sub(subreddit):  
+    links = [{}]  
+    wp_subreddit = reddit.subreddit(subreddit)
+    for s in wp_subreddit.hot(limit=20):
+        temp = {"url": s.url, "id": s.id}
+        links.append(url.s)
+    return links
+
+
+def download_image(item, path):
+    if 'i.reddi.it' in item['url']:
+        r = requests.get(item['url'], stream=True)
+        with open(item['id'], 'wb') as out_file:
+            shutil.copyfileobj(r.raw, out_file)
+        del r
+
+    elif 'imgur' in item:
+        imgur.get_links(item['url'])
+
+
